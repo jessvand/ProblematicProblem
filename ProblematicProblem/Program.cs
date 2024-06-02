2afc344119c04eef3eeca9fd033472fd5912c366
jessvand <jmvandbeek@gmail.com>
2024-06-02 17:00:19 -0400

    Updated code to incl WineTasting when user is 21+

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -9,7 +9,7 @@ namespace ProblematicProblem
     {
         static Random rng = new Random();
         static bool cont = true;
-        static List<string> activities = new List<string>() { "Movies", "Paintball", "Bowling", "Lazer Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting" };
+        static List<string> activities = new List<string>() { "Movies" , "Paintball" , "Bowling" , "Lazer Tag" , "LAN Party" , "Hiking" , "Axe Throwing" , "Wine Tasting"  };
 
         static void Main(string[] args)
         {
@@ -124,7 +124,7 @@ namespace ProblematicProblem
                 int randomNumber = rng.Next(activities.Count);
                 string randomActivity = activities[randomNumber];
 
-                if (userAge > 21 && randomActivity == "Wine Tasting")
+                if (userAge >= 21 && randomActivity == "Wine Tasting")
                 {
                     Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
                     Console.WriteLine("Please choose something else!");
